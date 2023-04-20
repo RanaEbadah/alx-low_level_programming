4155cf8fada5eba2e3f58905aaf401c1b28b3dbe
RanaEbadah <ranaebadah@hotmail.com>
2023-04-20 14:50:06 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/*
+ * This is the entry point of my first program
+ */
+int main(void) /* the main function has a return value int*/
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); /* Thi is the end of my program */
+}
