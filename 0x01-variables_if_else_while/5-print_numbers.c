4d4e01d640f4e0f9d608000fa89730a17a86e4f5
RanaEbadah <ranaebadah@hotmail.com>
2023-04-24 10:03:12 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - a prgram that print numbers
+* Return: 0 always zero
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("/n");
+}
