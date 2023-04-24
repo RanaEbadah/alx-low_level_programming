f9e5f1171f005b6722b673c6859add3cbf0ebe9b
RanaEbadah <ranaebadah@hotmail.com>
2023-04-24 04:54:22 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+* main - a prgram that the alphabet small case
+* Return: 0 always zero
+*/
+int main(void)
+{
+	char alphas[27];
+	int i;
+
+	for (i = 0; i < 28; i++)
+	{
+		alphas[i] = 'a' + i;
+		putchar(alphas[i]);
+	}
+	return (0);
+}
