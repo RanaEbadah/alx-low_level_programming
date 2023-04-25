8d8c4ecdde8c2fee557d8f5b219f1ffac134c344
RanaEbadah <ranaebadah@hotmail.com>
2023-04-25 16:28:14 -0700

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,28 +1,5 @@
 #include "main.h"
 /**
- *print - printsint using _putchar
- *@n: the number that will be print
- */
-
-void print(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	if (n / 10)
-	{
-		print(n / 10);
-	}
-	_putchar((n % 10) + '0');
-}
-
-/**
  *add - add two integers
  *@x: The first int.
  *@y: The second int
@@ -35,7 +12,6 @@ int add(int x, int y)
 	int result;
 
 	result = x + y;
-	print(result);
 	return (result);
 
 }
