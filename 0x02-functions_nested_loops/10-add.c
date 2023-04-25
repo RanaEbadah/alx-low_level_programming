ce59047be386f6536e70dada0c017a049ca57b2d
RanaEbadah <ranaebadah@hotmail.com>
2023-04-25 15:57:07 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *print - printsint using _putchar
+ *@n: the number that will be print
+ */
+
+void print(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
+
+/**
+ *add - add two integers
+ *@x: The first int.
+ *@y: The second int
+ *Return: The result
+ *
+ */
+
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+	print(result);
+	return (result);
+
+}
