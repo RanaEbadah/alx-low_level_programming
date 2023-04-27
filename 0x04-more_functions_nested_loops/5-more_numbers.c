9c42617cde96a3d57cc413f90b4744f22e065c66
RanaEbadah <ranaebadah@hotmail.com>
2023-04-27 06:48:34 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,14 +14,13 @@ void more_numbers(void)
 
 	for (j = 0; j <= 10; j++)
 	{
-		for (i = 48; i <= 57; i++)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i);
-		}
-		for (k = 10; k <= 14; k++)
-		{
-			_putchar('1');
-			_putchar((k % 10) + 48);
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
 		}
 
 		_putchar('\n');
