175938ffa5f7128b1cc84345dec5e9846fb3d4c4
RanaEbadah <ranaebadah@hotmail.com>
2023-04-27 10:53:07 -0700

    u

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,19 +1,4 @@
-#include <unistd.h>
 #include "main.h"
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 
 /**
 *print_square -  prints a square, followed by a new line
@@ -24,7 +9,7 @@ int _putchar(char c)
 
 void print_square(int size)
 {
-	int i;
+	int i, j;
 
 	if (size <= 0)
 	{
@@ -34,7 +19,11 @@ void print_square(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
 		_putchar('\n');
 	}
