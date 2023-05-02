a6598db057f4b66426281d46f0062940e52b062e
root <root@DESKTOP-CQLQUBT.localdomain>
2023-05-02 18:07:15 +0400

    g

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,11 +18,11 @@ void print_array(int *a, int n)
 		{
 			if (i != n - 1)
 			{
-				printf("%d, ", *(a + i));
+				printf("%d, ", a[i]);
 			}
 			else
 			{
-				printf("%d\n", *(a + i));
+				printf("%d\n", a[i]);
 			}
 		}
 	}
