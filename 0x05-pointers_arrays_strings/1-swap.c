4e9795f118b74b1b4b3f5410e80d6e8a2eb85be4
root <root@DESKTOP-CQLQUBT.localdomain>
2023-05-02 11:37:07 +0400

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,12 +7,13 @@
  *
  */
 
- void swap_int(int *a, int *b)
- {
-    int a1, b1;
-    a1 = *a;
-    b1 = *b;
+void swap_int(int *a, int *b)
+{
+	int a1, b1;
 
-    *a = b1;
-    *b = a1;
- }
+	a1 = *a;
+	b1 = *b;
+
+	*a = b1;
+	*b = a1;
+}
