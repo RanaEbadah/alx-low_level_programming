3298027d0ab7e6e989987aa8c88484682220003c
root <root@DESKTOP-CQLQUBT.localdomain>
2023-05-04 20:28:18 +0400

    try

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,40 +10,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int s1length = 0;
-	int s2length = 0;
-	int nInt = 0;
+
 	int i = 0;
 
-	while (s1[s1length] != '\0')
+	while ((s1[i] != '\0') && (s2[i] != '\0'))
 	{
-		s1length++;
-	}
-	while (s2[s2length] != '\0')
-	{
-		s2length++;
-	}
-	if (s1length > s2length)
-	{
-		nInt = s1length - s2length;
-	}
-	else if (s1length < s2length)
-	{
-		nInt = s1length - s2length;
-	}
-	else
-	{
-		for (i = 0; i < s1length; i++)
+		if (s1[i] != s2[i])
 		{
-			if (s1[i] == s2[i])
-			continue;
-			else
-			{
-				nInt = s1[i] - s2[i] ;
-				break;
-			}
-		}
-	}
-	return (nInt);
+			return (s1[i] - s2[i]);
 
+		}
+		i++;
+	}
+
+	return (0);
 }
