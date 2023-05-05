57456aab7e0b29c900d1e707abf624429b52421b
root <root@DESKTOP-CQLQUBT.localdomain>
2023-05-05 06:40:15 +0400

    g

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *infinite_add - add two integers.
+ *@n1: int number 1 input.
+ *@n2: int number 1 input.
+ *@r: buffer.
+ *@size_r: buffer size.
+ *Return: pointer to string.
+ *
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+{
+
+}
