7d65d6381b87c766703b1dea4616d20fccec8bba
root <root@DESKTOP-CQLQUBT.localdomain>
2023-05-08 14:47:18 +0400

    task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,7 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	char *result;
+	char *result = '\0';
 
 	while (s[i] != '\0')
 	{
@@ -21,7 +21,7 @@ char *_strchr(char *s, char c)
 		}
 		i++;
 	}
-	return ('\0');
+	return (result);
 
 
 
