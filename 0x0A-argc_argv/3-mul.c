9ab7da21d67c9c105a9efbc48a8fb7ed9a660e43
root <root@DESKTOP-CQLQUBT.localdomain>
2023-05-14 13:30:58 +0400

    f

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+*main - a program that mutiply two numbers
+*@argc: int the number of arguments.
+*@argv: array of strings
+*Return: 0 always zero
+*/
+
+int main(int argc, char *argv[])
+{
+	int x = 0, y = 0;
+
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+
+	printf("%d\n", x * y);
+
+	return (0);
+
+
+}
