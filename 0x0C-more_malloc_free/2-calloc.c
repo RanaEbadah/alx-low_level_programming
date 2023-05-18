2e0abece7dcfd135d294a361654ecde6fe040299
root <root@DESKTOP-CQLQUBT.localdomain>
2023-05-18 20:48:54 +0400

    g

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,29 +3,48 @@
 #include<stdlib.h>
 
 /**
+*_memset - initialize memory space
+*@ptr: pointer
+*@c: int
+*@num: int
+*Return: pointer
+*
+*/
+
+void *_memset(void *ptr, int c, int num)
+{
+	char *p = ptr;
+	int i;
+
+	for (i = 0; i < num; i++)
+	{
+		p[i] = c;
+	}
+
+	return (ptr);
+}
+
+/**
 *_calloc - allocates memory for an array, using malloc
 *@nmemb: first int
 *@size: second int
 *Return: pointer
 *
 */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
-	unsigned int i;
+	void *p;
 
 	if (nmemb == 0 || size == 0)
 	return (NULL);
 
-	p = (char *)malloc(nmemb * size);
+	p = malloc(nmemb * size);
 
 	if (p == NULL)
 	return (NULL);
 
-	for (i = 0; i < nmemb; i++)
-	{
-		p[i] = 0;
-	}
+	p = _memset(p, 0, nmemb);
 
 	return (p);
 }
