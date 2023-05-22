a03841f24df99cde9c763982d8918ef87acb4efa
root <root@DESKTOP-CQLQUBT.localdomain>
2023-05-22 22:56:02 +0400

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef GOG_H
+#define GOG_H
+
+/**
+ * struct dog - dog data type
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* GOG_H */
