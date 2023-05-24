92b2a56cc26c759e82a231d171defaa04344c913
root <root@DESKTOP-CQLQUBT.localdomain>
2023-05-24 21:09:13 +0400

    g

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include<stdio.h>
+#include<stdlib.h>
 
 /**
 *print_name -  prints a name.
@@ -10,5 +12,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if ((name != NULL) && (f != NULL))
 	f(name);
 }
