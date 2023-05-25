337809b2a47416dfc5c34c524c4b043dc8abd068
root <root@DESKTOP-CQLQUBT.localdomain>
2023-05-26 03:33:03 +0400

    h

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -35,7 +35,7 @@ void print_all(const char * const format, ...)
 			case 's':
 				str = va_arg(args, char*);
 				if (str == NULL)
-				str ="(nil)";
+				str = "(nil)";
 				printf("%s%s", separator, str);
 				break;
 			default:
