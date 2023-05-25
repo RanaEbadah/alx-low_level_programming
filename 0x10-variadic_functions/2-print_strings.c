5713bacf050944da386090a56fbdfd5068a7f941
root <root@DESKTOP-CQLQUBT.localdomain>
2023-05-26 02:24:02 +0400

    h

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,5 +10,24 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    unsigned int i;
+	unsigned int i;
+	va_list args;
+	char *str;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char*);
+		if (str == NULL)
+		printf("%s", "(nil)");
+		else
+		printf("%s", str);
+
+		if ((i != n - 1) && (separator != NULL))
+		printf("%s", separator);
+
+	}
+	printf("\n");
+	va_end(args);
 }
