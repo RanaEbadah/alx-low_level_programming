6ae1de9ca5492c8ec95c672bfc1233accf29d422
root <root@DESKTOP-CQLQUBT.localdomain>
2023-05-26 03:06:36 +0400

    h

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+
+/**
+*print_all - prints anything.
+*@format: string.
+*Return: void
+*
+*/
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *str;
+	char *separator;
+
+	va_start(args, format);
+	while (format[i])
+	{
+		if (i == 0)
+		separator = "";
+		else
+		separator = ", ";
+		if (!format)
+		return;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char*);
+				if (str == NULL)
+				printf("%s(nil)", separator);
+				else
+				printf("%s%s", separator, str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
