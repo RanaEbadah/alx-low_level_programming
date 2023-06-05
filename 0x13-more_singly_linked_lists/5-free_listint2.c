a3a67f57899c45cda80fb147736b7f498cf24d53
Rana Ebadah <ranaebadah@hotmail.com>
2023-06-06 01:28:18 +0400

    g

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,9 +13,9 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 	return;
 
-	while (*head != NULL)
+	while (*head)
 	{
-		*head = ((*head)->next);
+		node = ((*head)->next);
 		free(*head);
 		*head = node;
 	}
