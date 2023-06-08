83e3eea433c15640a0bf62d0b7cd6b33793f42b9
Rana Ebadah <ranaebadah@hotmail.com>
2023-06-09 00:11:36 +0400

    h

--- 0x14-bit_manipulation/1-main.c
+++ 0x14-bit_manipulation/1-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <limits.h>
 
 /**
  * main - check the code
@@ -17,6 +18,10 @@ int main(void)
     print_binary(1024);
     printf("\n");
     print_binary((1 << 10) + 1);
+    printf("\n");
+    print_binary(INT_MAX);
+    printf("\n");
+     print_binary((1 << 20) + 1);
     printf("\n");
     return (0);
 }
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*get_bit - returns the value of a bit at a given index.
+*@n: the int
+*@index: the index
+*Return: void
+*
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int checker;
+	int result;
+
+	checker = 1 << index;
+	result = (checker & n) != 0;
+	return (result);
+
+}
