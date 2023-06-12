8c000721d8461aaf14b26e24954c220d563d5113
Rana Ebadah <ranaebadah@hotmail.com>
2023-06-12 21:55:13 +0400

    f

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+*read_textfile - reads a text file and prints it to the POSIX standard output.
+*@filename: file name.
+*@letters: size_t (the number of bytes)
+*Return: ssize_t
+*
+*/
