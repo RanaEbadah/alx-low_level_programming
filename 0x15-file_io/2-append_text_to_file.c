a230e9081fde14608fa259108fb4b085935ccf05
Rana Ebadah <ranaebadah@hotmail.com>
2023-06-12 22:07:21 +0400

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,9 +1,32 @@
 #include "main.h"
 
 /**
-*read_textfile - reads a text file and prints it to the POSIX standard output.
+*append_text_to_file -  appends text at the end of a file.
 *@filename: file name.
-*@letters: size_t (the number of bytes)
-*Return: ssize_t
+*@text_content: the string to be append
+*Return: int
 *
 */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, written, i = 0;
+
+	if (filename  == NULL)
+	return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+		i++;
+	}
+
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+	return (-1);
+
+	written = write(fd, text_content, i);
+	if (written == -1)
+	return (-1);
+
+	return (1);
+}
